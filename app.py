6adf30c6243560ccc36c5012010072bd8465eec3
Gogs <gogs@fake.local>
2019-03-27 14:33:13 +0100

    static dir

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello Ervin!"
+@app.route("/index")
+def index():
+    return app.send_static_file('index.html')
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
