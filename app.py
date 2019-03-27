ee8081e215471800fbc30a34b0607f14995495c3
Gogs <gogs@fake.local>
2019-03-27 14:46:09 +0100

    jinja

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, render_template
 app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello Ervin!"
 @app.route("/index")
 def index():
-    return app.send_static_file('index.html')
+    return render_template('layout.html',title="Brigitta")
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
